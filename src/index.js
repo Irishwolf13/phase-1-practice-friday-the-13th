a5c19bd47f750526f42a159ed7fc8c2ff573c0cc
John Rooney <rooneyjohn@gmail.com>
2022-12-21 20:51:41 -0500

    done and done

--- src/index.js
+++ src/index.js
@@ -1,75 +1,99 @@
+//wait for DOM to Load (not sure I really need this one this time)
 document.addEventListener("DOMContentLoaded", () => {
-    //get our stuff
-    let movieList = document.querySelector("#movie-list")
-    let movieTitle = document.querySelector("#title")
-    let movieYear = document.querySelector("#year-released")
-    let movieDescription = document.querySelector("#description")
-    let movieWatched = document.querySelector("#watched") 
-    let movieBloodAmount = document.querySelector("#amount")
-    let movieDetailImage = document.querySelector("#detail-image")
-    let currentWatched = false;
-    let myCurrentID = 0;
 
-    fetch(`http://localhost:3000/movies`)
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(element => {
-            let myImage = document.createElement('img')
-            myImage.setAttribute('src', element.image)
-            
-            myImage.addEventListener('click', (myEvent) => {
-                myCurrentID = element.id;
-                movieDetailImage.setAttribute('src', element.image)
-                movieTitle.innerHTML = element.title;
-                movieYear.innerHTML = element.release_year;
-                movieDescription.innerHTML = element.description;
-                movieBloodAmount.innerHTML = element.blood_amount;
-                currentWatched = element.watched;
-                fetch(`http://localhost:3000/movies/${element.id}`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data.watched);
-                })
-            });
-            movieList.append(myImage)
+    const url = 'http://localhost:3000/movies'
+    let movieArray                              // *** Super Important to have this in global scope ***
+    let currentMovie                           // *** Super Important to have this in global scope ***
+
+    //Gather our minions
+    const movieList = document.querySelector('#movie-list')
+    const mainMovieImage = document.querySelector('#detail-image')
+    const mainMovieTitle = document.querySelector('#title')
+    const mainMovieYear = document.querySelector('#year-released')
+    const mainMovieDescription = document.querySelector('#description')
+    const watchStatusButton = document.querySelector('#watched')
+    const mainMovieBlood = document.querySelector('#amount')
+    const bloodForm = document.querySelector('#blood-form')
+    const myInput = document.querySelector('#blood-amount');
+
+    //Fetch all data from server
+    function getAllData() {
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            movieArray = data;
+            movieArray.map((movie) => {
+                addImageToNav(movie)
+            })       
+            setUpMainDisplay(movieArray[0]) //sets up original display
         })
-        //processes first movie
-        myCurrentID = 1;
-        movieDetailImage.setAttribute('src', data[0].image)
-        movieTitle.innerHTML = data[0].title;
-        movieYear.innerHTML = data[0].release_year;
-        movieDescription.innerHTML = data[0].description;
-        movieBloodAmount.innerHTML = data[0].blood_amount;
-        if (data[0].watched == false){
-            movieWatched.innerHTML = 'Unwatched';
-            currentWatched = data[0].watched;
-        }else {
-            movieWatched.innerHTML = 'Watched';
-            currentWatched = data[0].watched;
-        }
-    })
+    }
+    getAllData();
 
-    movieWatched.addEventListener('click', (e) => {
-        //Change watched status and change watched button text
-        if (currentWatched == true) {
-            currentWatched = false;
-            movieWatched.innerHTML = 'UNWATCHED'
-        }else if (currentWatched == false) {
-            currentWatched = true;
-            movieWatched.innerHTML = 'WATCHED'
-        }
+    //Adds images to top nav bar
+    const addImageToNav = (movie) => {
+        const movieImage = document.createElement('img');
+        movieImage.src = movie.image;
+        movieImage.alt = movie.title;
+        movieImage.addEventListener('click', () => posterClicked(movie)) //This is Key - It passes in the movie object that it recieved from getAllData()
+        movieList.append(movieImage)
+    }
+    //When movie poster is clicked
+    function posterClicked(movie) {
+        setUpMainDisplay(movie)
+    }
+
+    //Sets up main display for use
+    function setUpMainDisplay(movie) {
+        mainMovieImage.src = movie.image;
+        mainMovieTitle.innerHTML = movie.title
+        mainMovieYear.innerHTML = movie.year
+        mainMovieDescription.innerHTML = movie.description
+        mainMovieBlood.innerHTML = movie.blood_amount
         
-        //tell server what watched status is
-        fetch(`http://localhost:3000/movies/${myCurrentID}`, {
+        watchStatusButton.innerHTML = (movie.watched ? "Watched" : "Unwatched")
+        currentMovie = movie;
+    }
+    
+    // Adds event listener to my watch/unwatch button
+    watchStatusButton.addEventListener('click', () => {
+        clickedWatchButton(currentMovie)
+    })
+    // Adds function to my watch/unwatch button
+    function clickedWatchButton(movie) {
+        movie.watched = !movie.watched //This changes it from true to false and vice versa
+        watchStatusButton.innerHTML = (movie.watched ? "Watched" : "Unwatched") //This changes the Dom
+        //need Fetch to PATCH
+        fetch(`${url}/${movie.id}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             },
-            body: JSON.stringify({
-                watched: currentWatched
-            })
+            body: JSON.stringify(movie)
         })
-    })
+        // .then(response => response.json()) //These are just to check my work
+        // .then(movie => console.log(movie))
+    }
 
+    // Time to deal with the Blood amount
+    bloodForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        bloodFormClicked(currentMovie);
+    })
+    function bloodFormClicked(movie) {
+        let myFinalNumber = parseInt(myInput.value) + parseInt(mainMovieBlood.innerHTML); // Adds numbers together
+        mainMovieBlood.innerHTML = myFinalNumber;
+        movie.blood_amount = myFinalNumber; 
+        fetch(`${url}/${movie.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({blood_amount: myFinalNumber})
+        })
+        .then(response => response.json())
+        .then(movie => console.log("after ", movie.blood_amount))
+    }
 });
